166ad34f36484a58049ff4fcd2f9e47b861d7fb1
Navarro <navarrorangel1@hotmail.com>
2025-11-06 14:01:32 -0300

    :sparkles: feat: added buttons to change de porpertys

--- src/app/property-binding/property-binding.ts
+++ src/app/property-binding/property-binding.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
 export class PropertyBinding {
   text= "Texto do input";
   inputType = 'text';
+  inputDisabled = true;
+
+  changeTheState(){
+  this.inputDisabled = false
+  }
+  desabilitar(){
+    this.inputDisabled = true;
+  }
+  changeText(){
+    this.text = "mudou!"
+  }
+  changeInput(){
+    if(this.inputType == 'text'){
+      this.inputType = 'password';
+    } else {
+      this.inputType = 'text';
+    }
+  }
 }
