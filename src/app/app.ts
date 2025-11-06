c7a405cf3573ca37dfbcc98eaec34d138ac58eba
Navarro <navarrorangel1@hotmail.com>
2025-11-06 13:36:34 -0300

    :sparkles: feat: added property-Binding concept!

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import {MeuBotao} from './meu-botao/meu-botao';
 import {BotaoFlat} from './botao-flat/botao-flat';
 import {EventBinding} from './event-binding/event-binding';
+import {PropertyBinding} from './property-binding/property-binding';
 
 
 @Component({
   selector: 'app-root',
-  imports: [MeuBotao, BotaoFlat, EventBinding],
+  imports: [MeuBotao, BotaoFlat, EventBinding, PropertyBinding],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
