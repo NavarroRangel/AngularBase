4a8e9ed56b0adf13850ddfe541e40b33a1f38202
Navarro <navarrorangel1@hotmail.com>
2026-01-06 10:20:40 -0300

    :sparkles: feat: last component using class binding finished. Simple-visibility component

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,11 +9,12 @@ import {ProgressBar} from './components/style-binding/progress-bar/progress-bar'
 import {SquarePopup} from './components/style-binding/square-popup/square-popup';
 import {ActiveButton} from './components/class-binding/active-button/active-button';
 import {InvalidInput} from './components/class-binding/invalid-input/invalid-input';
+import {SimpleVisibility} from './components/class-binding/simple-visibility/simple-visibility';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ProgressBar, SquarePopup, ActiveButton, InvalidInput],
+  imports: [ProgressBar, SquarePopup, ActiveButton, InvalidInput, SimpleVisibility],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/class-binding/simple-visibility/simple-visibility.ts
+++ src/app/components/class-binding/simple-visibility/simple-visibility.ts
@@ -3,9 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-simple-visibility',
   imports: [],
-  templateUrl: './simple-visibility.html',
-  styleUrl: './simple-visibility.css'
+  template: `
+    <div [class.hidden]="isHidden" class="message-box">
+      Esse texto pode ser Ocultado
+    </div>
+    <button (click)="toggleVisibility()">
+      {{isHidden ? 'Mostrar' : 'Ocultar'}}  Texto
+    </button>
+
+  `,
+  styles: `
+  .message-box {
+    padding: 15px;
+    margin: 10px;
+    border: 1px solid #007bff;
+    background-color: #e7f3fe;
+    color: #007bff;
+    transition: opacity 0.3s ease;
+
+  }
+  .hidden{
+    opacity: 0 ;
+  }
+
+  button{
+   padding: 8px 12px;
+    margin: 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: #6c757d;
+    color: white;
+    cursor: pointer;
+
+  }
+  `
 })
 export class SimpleVisibility {
+  isHidden : boolean = false;
 
+  toggleVisibility () {
+    this.isHidden = !this.isHidden;
+  }
 }
